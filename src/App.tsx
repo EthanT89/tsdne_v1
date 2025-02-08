9007eece8750c0e4b81f434d5304806188c6c956
Ethan Thornberg <ethan.l.thornberg@gmail.com>
2025-02-07 16:57:35 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+  const items = ["Lake Arrowhead", "Los Angeles", "Crestline", "Tulsa"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          Hello<span>World</span>
+        </Alert>
+      )}
+      <Button color="danger" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
+
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
+}
+
+export default App;
