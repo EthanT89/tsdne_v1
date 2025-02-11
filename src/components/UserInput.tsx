e52743abfecd45a86cac7d356011e3b6b3164a06
Ethan Thornberg <ethan.l.thornberg@gmail.com>
2025-02-10 18:52:45 -0800

    Base UserInput Component

---
+++ src/components/UserInput.tsx
@@ -0,0 +1,26 @@
+interface UserInputProps {
+  input: string;
+  setInput: (value: string) => void;
+  onSubmit: () => void;
+}
+
+const UserInput = ({ input, setInput, onSubmit }: UserInputProps) => {
+  return (
+    <div className="w-3/4 mt-4 flex">
+      <input
+        className="w-full p-3 bg-gray-700 text-white rounded-lg placeholder-gray-400 border border-white focus:outline-none focus:ring focus:ring-blue-500"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="What's next..."
+      />
+      <button
+        className="ml-2 p-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
+        onClick={onSubmit}
+      >
+        Go
+      </button>
+    </div>
+  );
+};
+
+export default UserInput;
