2d34479745ac38c4caf8c47cbc16f00ea90956da
Ethan Thornberg <ethan.l.thornberg@gmail.com>
2025-02-10 19:06:53 -0800

    Fixed Links & Imported Assets

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,33 @@
 const Footer = () => {
   return (
     <footer className="absolute bottom-4 flex justify-between w-full px-6 text-white">
-      <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://linkedin.com/ethan-thornberg/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img
           src="/linkedin.svg"
           alt="LinkedIn"
           className="h-8 w-8 opacity-75 hover:opacity-100"
         />
       </a>
-      <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://github.com/EthanT89"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img
           src="/github.svg"
           alt="GitHub"
           className="h-8 w-8 opacity-75 hover:opacity-100"
         />
       </a>
-      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://www.instagram.com/ethanthornberg/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img
           src="/instagram.svg"
           alt="Instagram"
